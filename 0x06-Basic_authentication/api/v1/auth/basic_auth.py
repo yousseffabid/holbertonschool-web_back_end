5721d506a055c52e277a71b0c185bf1de4c7669c
yousseffabid <abidyoussef58@gmail.com>
2023-02-07 22:13:47 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,7 +32,7 @@ class BasicAuth(Auth):
                 return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> Tuple[str]:
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
         '''extract_user_credentials'''
         if (decoded_base64_authorization_header and
                 isinstance(decoded_base64_authorization_header, str) and
